0f489ecbf91cb56ce4b77437b55820170ff03c0a
Yesid Moreno <yesidmoreno@MacBook-Pro-de-Yesid.local>
2025-09-13 16:12:52 -0500

    Initial commit

---
+++ src/test/java/dev/yesidmoreno/client_management/ClientManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yesidmoreno.client_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
